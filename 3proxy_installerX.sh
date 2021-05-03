d26deed2bd8291732a7119d135844a7b600f6d5f
bomzene <83580247+bomzene@users.noreply.github.com>
2021-05-03 10:54:38 +0100

    Update 3proxy_installerX.sh

--- 3proxy_installerX.sh
+++ 3proxy_installerX.sh
@@ -8,12 +8,12 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://raw.githubusercontent.com/bomzene/bomproxies/master/3proxyX.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/bomzene/bomproxy/master/3proxyX.cfg
 chmod 600 /etc/3proxy/3proxyX.cfg
 mv 3proxyX.cfg 3proxy.cfg
-wget --no-check-certificate https://raw.githubusercontent.com/bomzene/bomproxies/master/.proxyauth
+wget --no-check-certificate https://raw.githubusercontent.com/bomzene/bomproxy/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate https://raw.githubusercontent.com/bomzene/bomproxies/master/3proxyinit
+wget --no-check-certificate https://raw.githubusercontent.com/bomzene/bomproxy/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 update-rc.d 3proxyinit defaults
